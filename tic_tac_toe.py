3abb4f75e216e452f364a50d8b5adfef19a91d1a
dlasing <46413988+dlasing@users.noreply.github.com>
2019-01-28 17:16:19 -0800

    Add files via upload
    Added display_board() and player_input()

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,17 +1,49 @@
-#print('\n'*100)
+print ('\n'*100)
 
 def display_board(board):
+
+	print ('   |   |   \n'   \
+	       f' {board[7]} | {board[8]} | {board[9]}   \n'   \
+	       '   |   |   \n'   \
+	       '-----------\n'   \
+	       '   |   |   \n'   \
+	       f' {board[4]} | {board[5]} | {board[6]}   \n'   \
+	       '   |   |   \n'   \
+	       '-----------\n'   \
+	       '   |   |   \n'   \
+	       f' {board[1]} | {board[2]} | {board[3]}   \n'   \
+	       '   |   |   \n')
 	
-	bar = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]	
-	row = '-----------'
+
+
+
+def play_input():
+	player_list = ["?", "?"]
+	print(type(player_list))
+	
+	print (player_list[0])
+	
+	while player_list[0] != 'X' or player_list[0] != "O":
+		player_list[0] = input("Please pick a marker 'X' or 'O' ")
 		
-	for print_row in range (1,4):
-		for print_bar in range (1,4):
+		if player_list[0] == "X":
+			player_list[1] = "O"
+			return player_list
+			break
+		if player_list[0] == "O":
+			player_list[1] = "X"
+			return player_list
+			break
 			
-			print (bar[0] + bar[1] + bar[2] + bar[3] +bar[4] + bar[5] + bar[6] + bar[7]+ bar[8] + bar[9] )
-						
-		if print_row < 3:
-			print (row)
+			
+		
+		
+			
 
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+display_board(test_board)
+x = play_input()
 
-display_board ('a')
+print (f'Player 1 is: {x[0]}')
+print (f'Player 2 is: {x[1]}')
+
