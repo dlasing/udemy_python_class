72625219a1155b3d9fb94d9167ea9d046168041e
Dennis <ultraman97@yahoo.com>
2019-01-29 22:38:37 -0800

    Finished

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -20,9 +20,8 @@ def display_board(board):
 
 
 
-def player_input():
+def player_input():	
 	player_list = ["?", "?"]
-	
 	
 	while player_list[0] != 'X' or player_list[0] != "O":
 		player_list[0] = input("Player1: Please pick a marker 'X' or 'O' ")
@@ -31,6 +30,7 @@ def player_input():
 			player_list[1] = "O"
 			return player_list
 			break
+			
 		if player_list[0] == "O":
 			player_list[1] = "X"
 			return player_list
@@ -38,39 +38,34 @@ def player_input():
 			
 			
 
-def place_marker(board, marker, position):
-	
+def place_marker(board, marker, position):	
 	# Assign the board list with new position
 	board[position] = marker
 	return board
 
 		
-def win_check(board, mark):
-	
+def win_check(board, mark):	
+	# A list to check all possible winning matrix
 	check_list = [0,1,2,3,4,5,6,7,8,9,1,4,7,2,5,8,3,6,9,3,5,7,1,5,9]
 	counter = 0
 	
 	for item in range(1,9):
 		
-		#print (f'Checking... {check_list[1+counter]}, {check_list[2+counter]}, {check_list[3+counter]}')
-		if board[check_list[1+counter]] == board[check_list[2+counter]] == board[check_list[3+counter]] == mark:
-			
-			
-			print (f"Marker: {mark} WON")
+		if board[check_list[1+counter]] == board[check_list[2+counter]] == board[check_list[3+counter]] == mark:				
+			print (f"Yeah! You just won the game!")
 			counter+=3
-			return True
-		
+			return True		
 		else:	
 			counter+=3
+	
+	
 			
 def choose_first():
 	player = random.randint(0,1)
 	return player			
 			
 
-
-def space_check(board, position):
-	
+def space_check(board, position):	
 		if board[position] == ' ':
 			return True
 		else:
@@ -92,17 +87,20 @@ def full_board_check(board):
 			
 def player_choice(board):
 	position = 0
-	while position < 1 or position > 9:
+	
+	while position < 1:
 		position = int(input ("Please enter your position [1-9]  "))
 	
-		
-	if space_check(board, position) == True:
-			return position
-		
-		
-		
-def replay():
+		# If the position is taken
+		if position > 9 or space_check(board, position) == False :
+			#Reset position
+			position = 0
 	
+	return position
+	
+				
+		
+def replay():	
 	play_again = ""
 	
 	while play_again != "N" or play_again != "Y":
@@ -112,7 +110,6 @@ def replay():
 	
 	
 def ready_to_play():
-	
 	yesno = "Yes"
 	
 	while yesno != "Yes" or yesno != "No":
@@ -132,12 +129,10 @@ print ("Welcome to Tic Tac Toe!")
 playagain = True
 
 while playagain == True:
-
-
 	# Start the Tic Tac Toe game
-	players = player_input()
-	print (f'Player 1 is: {players[0]}')
-	print (f'Player 2 is: {players[1]}\n')	
+	marker = player_input()
+	print (f'Player 1 is: {marker[0]}')
+	print (f'Player 2 is: {marker[1]}\n')	
 	
 	# Random pick which player will play first
 	who_is_first = choose_first()
@@ -149,10 +144,14 @@ while playagain == True:
 	play_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 	
 	while full_board_check(play_board) == False:
-		
-		play_board[player_choice(play_board)] = players[who_is_first]
+		display_board(play_board)
+		# Mark to the board
+		play_board[player_choice(play_board)] = marker[who_is_first]
 		display_board(play_board)
 		
+		# Any Winner ? 
+		if win_check(play_board, marker[who_is_first]) == True:
+			break
 		
 		# Step to switch player
 		if who_is_first == 0:
@@ -160,14 +159,6 @@ while playagain == True:
 		else:
 			who_is_first = 0
 			
-		
-		
-		
-	
-	
-	
-	
-	
 	playagain = replay()
 	
 	
@@ -178,19 +169,3 @@ while playagain == True:
 	
 	
 		
-#test_board = ['#','X','O','X','X','X','O','X','O','X']
-#display_board(test_board)
-#x = player_input()
-
-#new_board = place_marker(test_board, 'X', 9)
-#display_board(new_board)
-
-#win_check(new_board, 'X')
-
-#space_check(test_board, 4)
-
-#full_board_check(test_board)
-
-
-#print (f'Player 1 is: {x[0]}')
-#print (f'Player 2 is: {x[1]}')
