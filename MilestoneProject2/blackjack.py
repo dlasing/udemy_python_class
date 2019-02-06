e2ee48aeb8981b77833645ba8383b8ff2520d0d0
dlasing <ultraman97@yahoo.com>
2019-02-05 16:10:39 -0800

    complete the deck and card class  moving on

--- MilestoneProject2/blackjack.py
+++ MilestoneProject2/blackjack.py
@@ -1,22 +1,102 @@
 import random
+from colorama import Fore
+from colorama import Style
+
 
 # Declare fixed variable
-suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+#suits = ('Hearts', 'Clubs', 'Diamonds', 'Spades')
+suits = ('\u2665', '\u2663', '\u2666', '\u2660')
+ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10, 'A':11}
 playing = True
 
 
+def suit_color(suit):
+			
+	if suit == '\u2665' or suit == '\u2666':
+		return Fore.RED
+	else:
+		return Fore.BLACK
+
+ 
+
+class Deck():
+# The Class for the while deck with method of shuffle and deal	
+	def __init__(self):
+		self.deck = []
+		for suit in suits:
+			for rank in ranks:
+				self.deck.append([suit,rank])
+				
+		
+	def __str__(self):
+		print ('List of Deck')
+		for x in range(0,52):
+			code = suit_color(self.deck[x][0])	
+			print (f'{code}{self.deck[x][0]} {self.deck[x][1]} {Style.RESET_ALL}')
+							
+		return "DONE"
+
+
+	def shuffle(self):
+		random.shuffle(self.deck)
+		print ('shuffled')
+		for x in range(0,52):
+			code = suit_color(self.deck[x][0])	
+			print (f'{code}{self.deck[x][0]} {self.deck[x][1]} {Style.RESET_ALL}')
+
+
+
+	def deal(self):
+		single_card = self.deck.pop()
+		code = suit_color(single_card[0])	
+		print (f"single card is........{code}{single_card[0]}  {single_card[1]} {Style.RESET_ALL}")
+		return single_card
+		
+		
+
+class Card():
+	
+	def __init__(self, suit, rank):
+		self.suit = suit
+		self.rank = rank
+		
+	def __str__(self):
+		return self.suit + self.rank
+			
+	
+		
 
 while playing == True:
-	
+
 	suits_pick = suits[random.randrange(len(suits))]
 	ranks_pick = ranks[random.randrange(len(ranks))]
 	values_pick = values[ranks_pick]
 	
-	print (f'suits: {suits_pick}')
-	print (f'ranks: {ranks_pick}')	
-	print (f'values: {values_pick}')
+	#print (f'suits: {suits_pick}')
+	#print (f'ranks: {ranks_pick}')	
+	#print (f'values: {values_pick}')
+	
+	
+	a = Deck()
+	print (a)
+	
+	a.shuffle()
+	
+	print (a)
+	
+	got_it = a.deal()
+	b = Card(got_it[0], got_it[1])	
+	print (b)
+
+	
+	#a.deal()
+	
+	#b = Card(suits_pick, ranks_pick)
+	#print (b)
+
+	#mycard = Card(Deck)
+	#mycard.printcard()
 	
 	
 	x = input ('Play again? Y/N  ')
