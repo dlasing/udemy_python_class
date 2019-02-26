a2bf632e6e924eaa757da3bb8c69a7b74e418ad5
Dennis <ultraman97@yahoo.com>
2019-02-25 22:24:52 -0800

    inital code

---
+++ dailycode/code_0225.py
@@ -0,0 +1,18 @@
+# Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of 
+# ways it can be decoded. For example, the message '111' would give 3, since it could be 
+# decoded as 'aaa', 'ka', and 'ak'. You can assume that the messages are decodable. 
+# For example, '001' is not allowed.
+
+a = '111'
+alpha_list = []
+
+
+for x in range(0,len(a)):
+
+	alpha_list.append(a[x])
+	
+	
+for x in range(0,len(a)):
+	if 
+print (alpha_list)
+
