6d32ede827cbeef28baa9f3812905b2b26a26bc2
Dennis <ultraman97@yahoo.com>
2019-02-27 17:57:00 -0800

    done

--- dailycode/code_0225.py
+++ dailycode/code_0225.py
@@ -3,16 +3,26 @@
 # decoded as 'aaa', 'ka', and 'ak'. You can assume that the messages are decodable. 
 # For example, '001' is not allowed.
 
-a = '111'
-alpha_list = []
+a = '122991111'
+counter = 0
 
-
-for x in range(0,len(a)):
-
-	alpha_list.append(a[x])
+		
+for y in range(0,len(a)):
 	
+	if int(a[y]) == 0:
+		# if there is 0 in the string.. means per character
+		# match does not valid. Reset counter
+		counter = 0
+	else:
+		counter +=1
+
+for z in range(0,(len(a)-1)):	
 	
-for x in range(0,len(a)):
-	if 
-print (alpha_list)
+	if int(a[z:z+2]) <= 26:
+		print (f"one match: {a[z:z+2]}")
+		counter +=1
+	
+print (f"There are {counter} match")
+		
+		 
 
