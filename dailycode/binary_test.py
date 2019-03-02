917a98f91546a059083e81e49cbe4fa09b5b6ec0
dlasing <ultraman97@yahoo.com>
2019-03-01 16:07:43 -0800

    complete the binary tree

--- dailycode/binary_test.py
+++ dailycode/binary_test.py
@@ -1,4 +1,5 @@
-# This is the program to print the binary tree...
+# This is the program to print the binary tree level
+# And try to add up all value in the tree. 
 # Learn the structure of the binary Tree. 
 
 class Node:
@@ -14,7 +15,8 @@ class Node:
 def node_res(root):
 	
 	global call_level 
-	
+	global total
+		
 	# node_res() called, count the number of call
 	call_level += 1
 	
@@ -29,8 +31,9 @@ def node_res(root):
 		print (f"This is level: {call_level}")
 		print (f"root is: {root.root}")
 		print (f"left is: {root.left}")
-		print (f"right is: {root.right}\n\n")
- 
+		print (f"right is: {root.right}")
+		total = total + root.root
+		print (f"total is.... {total}\n\n")
 		
 	# Check the node left and right leave.  
 	#         root
@@ -40,22 +43,31 @@ def node_res(root):
 	left  = node_res(root.left)
 	right = node_res(root.right)
 	
+	return total	
+	
+	
+	
+def node_total(root):
+	
+		return node_res(root)
+
+
+
 
 # gloal value
-
 call_level = 0	
-	
+total = 0
 
 
 
-	
+# Build the Nodes...	
 root = Node(5)
 root.left  = Node(4)
 root.right = Node (3)
-root.left.left = Node(2)
-root.left.right = Node(9)
+#root.left.left = Node(2)
+#root.left.right = Node(9)
 
 
 
 
-node_res(root)
+print (f"The total of the tree is... {node_total(root)}")
